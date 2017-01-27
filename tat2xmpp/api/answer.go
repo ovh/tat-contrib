38a0fec941bdf5e6c53505c7b144eaf9656427e6
Yvonnick Esnault <yvonnick.esnault@corp.ovh.com>
2017-01-28 00:07:20 +0100

    fix(tat2xmpp): req tat
    Signed-off-by: Yvonnick Esnault <yvonnick.esnault@corp.ovh.com>

--- tat2xmpp/api/answer.go
+++ tat2xmpp/api/answer.go
@@ -107,9 +107,9 @@ func (bot *botClient) requestTat(in, remote string) string {
 
 	topic := tuple[1]
 	var values url.Values
-	if strings.Contains(in, "?") {
+	if strings.Contains(topic, "?") {
 		tuple2 := strings.Split(in, "?")
-		if len(tuple) != 2 {
+		if len(tuple2) != 2 {
 			return "invalid request. Request have to contains ?, example COUNT"
 		}
 		topic = tuple2[0]
