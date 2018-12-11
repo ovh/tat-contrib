1acdbaa90f00b8c4b075030890bf3a15892fa880
Fabien Meurillon <fabien.meurillon@corp.ovh.com>
2018-12-11 17:01:01 +0100

    Add indexer multiplexer

--- tat2es/api/client.go
+++ tat2es/api/client.go
@@ -1,14 +1,22 @@
 package main
 
 import (
+	"strconv"
+	"strings"
+
 	log "github.com/Sirupsen/logrus"
-	elastigo "github.com/mattbaird/elastigo/lib"
+	"github.com/mattbaird/elastigo/lib"
 	"github.com/ovh/tat"
 	"github.com/spf13/viper"
 )
 
 var instance *tat.Client
-var esConn *elastigo.Conn
+
+type esConn struct {
+	*elastigo.Conn
+	pause int
+	index string
+}
 
 // getClient initializes client on tat engine
 func getClient() *tat.Client {
@@ -38,16 +46,50 @@ func getClient() *tat.Client {
 	return instance
 }
 
-// getClient initializes client on tat engine
-func getClientES() *elastigo.Conn {
-	if esConn != nil {
-		return esConn
-	}
-	esConn = elastigo.NewConn()
-	esConn.Domain = viper.GetString("host_es")
-	esConn.Port = viper.GetString("port_es")
-	esConn.Username = viper.GetString("user_es")
-	esConn.Password = viper.GetString("password_es")
+// getClientsES initializes ES clients
+func getClientsES() ([]esConn, error) {
+	var esConns []esConn
 
-	return esConn
+	protocols := strings.Split(viper.GetString("protocol_es"), ",")
+	hosts := strings.Split(viper.GetString("host_es"), ",")
+	ports := strings.Split(viper.GetString("port_es"), ",")
+	users := strings.Split(viper.GetString("user_es"), ",")
+	passwords := strings.Split(viper.GetString("password_es"), ",")
+	indices := strings.Split(viper.GetString("force_index_es"), ",")
+	pauses := strings.Split(viper.GetString("pause_es"), ",")
+
+	for i, host := range hosts {
+		c := elastigo.NewConn()
+		c.Domain = host
+		c.Protocol = getStringValue(protocols, i)
+		c.Port = getStringValue(ports, i)
+		c.Username = getStringValue(users, i)
+		c.Password = getStringValue(passwords, i)
+		pause, err := getIntValue(pauses, i)
+		if err != nil {
+			return nil, err
+		}
+		esConns = append(esConns, esConn{Conn: c, pause: pause, index: getStringValue(indices, i)})
+	}
+	return esConns, nil
+}
+
+func getStringValue(array []string, index int) string {
+	if len(array) == 0 {
+		return ""
+	}
+	if index >= len(array) {
+		return array[len(array)-1]
+	}
+	return array[index]
+}
+
+func getIntValue(array []string, index int) (int, error) {
+	if len(array) == 0 {
+		return 0, nil
+	}
+	if index >= len(array) {
+		return strconv.Atoi(array[len(array)-1])
+	}
+	return strconv.Atoi(array[index])
 }
